aab2d9b0a257bcf04c3eef94b3a04b80c4fd141a
IsaacDdrCondor <116840108+IsaacDdrCondor@users.noreply.github.com>
2022-11-11 14:42:09 -0600

    Added tweaks

---
+++ components/MainContent.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="h-screen flex w-sreen block">
+    <div class="flex w-screen">
+      <div class="mx-auto">
+        <nuxt-img class="mx-auto w-50 px-12 py-12" src="/assets/logo.webp" />
+        <div
+          class="bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-50 bg-black mx-12 rounded-lg drop-shadow-md"
+        >
+          <h4
+            class="py-4 md:mx-54 mb-8 mx-8 font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-red-500 to-yellow-600 text-center drop-shadow-md"
+          >
+            Somos un proyecto colectivo que pretende cambiar el estigma social
+            relacionado con el cannabis.
+          </h4>
+        </div>
+        <div class="flex mb-10">
+          <div
+            class="inline-grid grid-cols-3 gap-6 w-full justify-content items-center"
+          >
+            <span class="w-12 mx-auto">
+              <a href="https://www.facebook.com/Frecuencia-420-107148185351025">
+                <nuxt-img class="drop-shadow-md" src="/assets/facebook.svg" />
+              </a>
+            </span>
+            <span class="w-12 mx-auto">
+              <a
+                href="https://www.youtube.com/channel/UCGvPTttw1IevOa4ivAfPEUQ"
+              >
+                <nuxt-img class="drop-shadow-md" src="/assets/youtube.svg" />
+              </a>
+            </span>
+
+            <span class="w-12 mx-auto">
+              <a href="https://www.twitch.tv/frecuencia420">
+                <nuxt-img class="drop-shadow-md" src="/assets/twitch.svg" />
+              </a>
+            </span>
+
+            <span class="w-12 mx-auto">
+              <a href="https://www.tiktok.com/@frecuencia420">
+                <nuxt-img class="drop-shadow-md" src="/assets/tiktok.svg" />
+              </a>
+            </span>
+
+            <span class="w-12 mx-auto">
+              <a href="https://www.instagram.com/lafrecuencia420/">
+                <nuxt-img class="drop-shadow-md" src="/assets/instagram.svg" />
+              </a>
+            </span>
+
+            <span class="w-12 mx-auto">
+              <a href="https://twitter.com/Frecuencia420">
+                <nuxt-img class="drop-shadow-md" src="/assets/twitter.svg" />
+              </a>
+            </span>
+
+            <span></span>
+
+            <span class="w-12 mx-auto">
+              <a href="https://soundcloud.com/frecuencia-420">
+                <nuxt-img class="drop-shadow-md" src="/assets/soundcloud.svg" />
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,67 +1,7 @@
 <!-- Please remove this file from your project -->
 <template>
-  <div class="h-screen flex w-sreen" id="vanta-selector">
-    <div class="flex w-screen">
-      <div class="mx-auto">
-        <nuxt-img class="mx-auto w-50 px-12 py-12" src="/assets/logo.webp" />
-        <h4
-          class="md:mx-54 my-12 font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-green-500 to-yellow-500 text-center"
-        >
-          Somos un proyecto colectivo que pretende cambiar el estigma social
-          relacionado con el cannabis.
-        </h4>
-        <div class="flex mb-10">
-          <div
-            class="inline-grid grid-cols-3 gap-6 w-full justify-content items-center"
-          >
-            <span class="w-12 mx-auto">
-              <a href="https://www.facebook.com/Frecuencia-420-107148185351025">
-                <nuxt-img src="/assets/facebook.svg" />
-              </a>
-            </span>
-            <span class="w-12 mx-auto">
-              <a
-                href="https://www.youtube.com/channel/UCGvPTttw1IevOa4ivAfPEUQ"
-              >
-                <nuxt-img src="/assets/youtube.svg" />
-              </a>
-            </span>
-
-            <span class="w-12 mx-auto">
-              <a href="https://www.twitch.tv/frecuencia420">
-                <nuxt-img src="/assets/twitch.svg" />
-              </a>
-            </span>
-
-            <span class="w-12 mx-auto">
-              <a href="https://www.tiktok.com/@frecuencia420">
-                <nuxt-img src="/assets/tiktok.svg" />
-              </a>
-            </span>
-
-            <span class="w-12 mx-auto">
-              <a href="https://www.instagram.com/lafrecuencia420/">
-                <nuxt-img src="/assets/instagram.svg" />
-              </a>
-            </span>
-
-            <span class="w-12 mx-auto">
-              <a href="https://twitter.com/Frecuencia420">
-                <nuxt-img src="/assets/twitter.svg" />
-              </a>
-            </span>
-
-            <span></span>
-
-            <span class="w-12 mx-auto">
-              <a href="https://soundcloud.com/frecuencia-420">
-                <nuxt-img src="/assets/soundcloud.svg" />
-              </a>
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
+  <div id="vanta-selector">
+    <MainContent />
   </div>
 </template>
 
